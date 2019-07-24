50cb4f322cb92a31992475ba18cd9a766739b53f
Lars-Jørgen Kristiansen <larsjk.84@gmail.com>
2019-07-24 22:46:54 +0200

    Initial

--- Package.swift
+++ Package.swift
@@ -18,9 +18,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(name: "Clibmodbus", pkgConfig: "libmodbus", providers: [
+            .brew(["libmodbus"]),
+            .apt(["libmodbus-dev"])
+        ]),
         .target(
             name: "Modbus",
-            dependencies: []),
+            dependencies: ["Clibmodbus"]),
         .testTarget(
             name: "ModbusTests",
             dependencies: ["Modbus"]),
--- Tests/ModbusTests/ModbusTests.swift
+++ Tests/ModbusTests/ModbusTests.swift
@@ -6,7 +6,7 @@ final class ModbusTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Modbus().text, "Hello, World!")
+//        XCTAssertEqual(Modbus().text, "Hello, World!")
     }
 
     static var allTests = [
